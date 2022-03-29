5e09e52ca68536029b846777a372437bd809bc45
ZeguangXiao <zeguangxiao@163.com>
2022-03-29 10:25:47 +0800

    RestTemplate test

--- business/src/main/java/com/qfedu/business/controller/HelloController.java
+++ business/src/main/java/com/qfedu/business/controller/HelloController.java
@@ -1,12 +1,21 @@
 package com.qfedu.business.controller;
 
+import com.qfedu.service.demo.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author XZG
@@ -14,6 +23,9 @@ import java.util.List;
  */
 @RestController
 public class HelloController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Autowired
     DiscoveryClient discoveryClient;
@@ -49,5 +61,51 @@ public class HelloController {
         String host = serviceInstance.getHost();
         int port = serviceInstance.getPort();
         return "hello resttemplate";
+    }
+
+
+    @GetMapping("/hello4")
+    public void hello4(){
+        System.out.println("======================GET====================");
+        Book book = restTemplate.getForObject("http://storage/book/?id={1}", Book.class, 99);
+        System.out.println("book = " + book);
+
+        HashMap<String,Object> map = new HashMap();
+        map.put("id",99);
+        Book book1 = restTemplate.getForObject("http://storage/book/?id={id}", Book.class, map);
+        System.out.println("book1 = " + book1);
+
+        System.out.println("======================GetForEntity====================");
+        ResponseEntity<Book> entity = restTemplate.getForEntity("http://storage/book/?id={1}", Book.class, 88);
+
+        System.out.println("entity.getStatusCodeValue() = " + entity.getStatusCodeValue());
+        System.out.println("entity.getStatusCode() = " + entity.getStatusCode());
+        HttpHeaders headers = entity.getHeaders();
+        Set<String> keySet = headers.keySet();
+        for (String s : keySet) {
+            List<String> list = headers.get(s);
+            System.out.println("list = " + list);
+        }
+
+        Book body = entity.getBody();
+        System.out.println("body = " + body);
+
+        System.out.println("======================POST====================");
+        MultiValueMap<String, Object> req = new LinkedMultiValueMap<>();
+        req.add("id",77);
+        req.add("name","三国演义");
+        req.add("author","罗贯中");
+        Book book2 = restTemplate.postForObject("http://storage/book/", req, Book.class);
+        System.out.println("book2 = " + book2);
+
+        System.out.println("======================POST-forLocation====================");
+        URI uri = restTemplate.postForLocation("http://storage/book/add",book1);
+        System.out.println("uri = " + uri);
+
+        System.out.println("======================PUT====================");
+        restTemplate.put("http://storage/book/",book1);
+
+        System.out.println("======================DELETE====================");
+        restTemplate.delete("http://storage/book/{1}",66);
     }
 }
--- storage/src/main/java/com/qfedu/storage/BookController.java
+++ storage/src/main/java/com/qfedu/storage/BookController.java
@@ -1,77 +1,48 @@
 package com.qfedu.storage;
 
-import com.qfedu.demo.service.Book;
-import com.qfedu.service.api.IBookController;
+import com.qfedu.service.demo.Book;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @author 百泽
- * @公众号 Java架构栈
- *
- * Hystrix Resilience4j Sentinel
- *
+ * @author XZG
+ * @creatTime 2022-03-29
  */
 @Controller
-public class BookController implements IBookController {
+@RequestMapping("/book")
+public class BookController {
 
-    /**
-     * ids 是调用方法传来的参数，这个参数格式是自定义的
-     *
-     * 1,2,3,4,5
-     * @param ids
-     * @return
-     */
-    @GetMapping("/books")
+    @GetMapping("/")
     @ResponseBody
-    public List<Book> getBooksByIds(String ids) {
-        //调用方传来的id
-        List<Integer> list = Arrays.stream(ids.split(",")).map(id -> Integer.parseInt(id)).collect(Collectors.toList());
-        List<Book> books = new ArrayList<>();
-        for (Integer id : list) {
-            Book b = new Book();
-            b.setId(id);
-            books.add(b);
-        }
-        System.out.println("ids = " + ids);
-        return books;
-    }
-
-    @Override
-    public Book getBookById(Integer id) {
+    public Book getBookById(Integer id){
         Book book = new Book();
         book.setId(id);
-        System.out.println("getBookById:" + id);
         return book;
     }
 
-    @Override
-    public void deleteBookById(Integer id) {
-        System.out.println("id = " + id);
-    }
-
-    @Override
-    public Book addBook(Book book) {
-        return book;
-    }
-
-    @PostMapping("/add")
-    public String addBook2(@RequestBody Book book) {
-        System.out.println("addBook2 = " + book);
-        return "redirect:/index";
-    }
-
-    @Override
-    public void updateBook(Book book) {
+    //这里没有加 @RequestBody 注解，所以参数是 key-value 形式的
+    @PostMapping("/")
+    @ResponseBody
+    public void addBook(Book book){
         System.out.println("book = " + book);
     }
 
+    @PostMapping("/add")
+    public String addBook2(@RequestBody Book book){
+        return "redirect:/index";
+    }
+
+    @PutMapping("/")
+    @ResponseBody
+    public void updateBook(@RequestBody Book book){
+        System.out.println("book = " + book);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public void deleteBookById(@PathVariable Integer id){
+        Book book = new Book();
+        book.setId(id);
+        System.out.println("book = " + book);
+    }
 }
